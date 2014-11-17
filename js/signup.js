8415726761231fea1056e7d09b0d3f42e0f62b6f
Kevin Ly <kevinly7@uw.edu>
2014-11-16 19:46:42 -0800

    finished challenege

--- js/signup.js
+++ js/signup.js
@@ -15,7 +15,7 @@ document.addEventListener('DOMContentLoaded', function() {
         option.innerHTML = usStates[idx].name;
         option.value = usStates[idx].code;
         state.appendChild(option);
-    }
+    } //populate states list
 
     var occupation = document.getElementById('occupation');
     occupation.addEventListener('change', function() {
@@ -25,16 +25,87 @@ document.addEventListener('DOMContentLoaded', function() {
         } else {
             signup.elements['occupationOther'].style.display = 'none';
         }
-    });
+    }); //show occupationOther field
 
     var noButton = document.getElementById('cancelButton');
     noButton.addEventListener('click', function() {
         if (window.confirm("Are you sure you would like to leave this page?")) {
             window.location = 'http://google.com';
         }
-    });
+    }); //confirm leaving page
 
-    signup.addEventListener('submit', function() {
-
-    });
+    signup.addEventListener('submit', onSubmit);
 });
+
+function onSubmit(evt) {
+    try {
+        var valid = validateForm(this);
+    } catch(e) {
+        valid = false;
+    }
+
+    if (!valid && evt.preventDefault) {
+        evt.preventDefault();
+    }
+    evt.returnValue = valid;
+    return valid;
+}
+
+function validateForm(form) {
+    var requiredFields = ['firstName', 'lastName', 'address1', 'city', 'state', 'zip', 'birthdate'];
+    var idx;
+    var formValid = true;
+
+    if(document.getElementById('signup').elements['occupationOther'].style.display == 'block') {
+        requiredFields.push('occupationOther');
+    }
+
+    for (idx = 0; idx < requiredFields.length; ++idx) {
+        formValid &= validateRequiredFields(form.elements[requiredFields[idx]]);
+    }
+
+    return formValid;
+}
+
+function validateRequiredFields(field) {
+    var valid = false;
+
+    if (field.name == 'zip') {
+        var zipRegExp = new RegExp('^\\d{5}$');
+        valid = zipRegExp.test(field.value);
+    }
+    else if (field.name == 'birthdate') {
+        var value = field.value.trim();
+        if (value.length > 0) {
+            var birthdateMessage = document.getElementById('birthdateMessage');
+
+            var today = new Date();
+            var dob = new Date(field.value);
+            var yearsDiff = today.getFullYear() - dob.getUTCFullYear();
+            var monthsDiff = today.getMonth() - dob.getUTCMonth();
+            var daysDiff = today.getDate() - dob.getUTCDate();
+            if (monthsDiff < 0 || (0 == monthsDiff && daysDiff < 0)) {
+                yearsDiff--;
+            }
+            valid = yearsDiff >= 13;
+
+            if (!valid) {
+                birthdateMessage.innerHTML = 'You must be 13 years or older to sign up.';
+            } else {
+                birthdateMessage.innerHTML = '';
+            }
+        }
+    }
+    else {
+        var value = field.value.trim();
+        valid = value.length > 0;
+    }
+
+    if(valid) {
+        field.className = 'form-control';
+    } else {
+        field.className = 'form-control invalid';
+    }
+
+    return valid;
+};
